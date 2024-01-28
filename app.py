d1ee21d89e2803effb784c93b46116dabcba9d56
Jathin017 <jathinms7@gmail.com>
2024-01-28 02:54:05 -0500

    New app.py update

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify
 import base64
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 def read_obj_file(file_path):
     with open(file_path, 'rb') as file:
